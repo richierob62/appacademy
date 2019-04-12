1d7551b6f136eccd68588b01fa97de5da1665914
richierob62 <richierob62@gmail.com>
2019-04-11 20:08:30 -0400

    Fixed password

--- homework/W5D4/app_name/app/models/user.rb
+++ homework/W5D4/app_name/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
-  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password, length: { minimum: 6 }, allow_nil: true
 
   before_validation :ensure_session_token
 
@@ -26,6 +26,7 @@ class User < ApplicationRecord
   end
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
