62e44dd7500c78532772adb0b1917e8338a427fa
richierob62 <richierob62@gmail.com>
2019-07-16 18:25:40 -0400

    Iterate Linked List Backwards

--- dayswork/W19D2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ dayswork/W19D2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -6,11 +6,11 @@
 // Prompt:
 // -------
 //
-// Iterate over a Singly Linked List of primitives backwards. When finished, 
-// return a string representing the original linked list's values backwards 
+// Iterate over a Singly Linked List of primitives backwards. When finished,
+// return a string representing the original linked list's values backwards
 // in the following format:
 //
-//                             'A -> B -> C -> D' 
+//                             'A -> B -> C -> D'
 //
 // ------------
 // Constraints:
@@ -26,9 +26,86 @@
 // Let's code!
 // -----------
 
-function iterateAcrossLinkedListBackwards(linkedList) {
-    // TODO: Implement the iterateAcrossLinkedListBackwards function here
-
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
 }
 
-exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
+class Stack {
+  constructor() {
+    this.top = null
+    this.bottom = null
+    this.length = 0
+  }
+
+  push(val) {
+    let node
+
+    if (typeof val === 'object') {
+      node = new Node(val.value)
+    } else {
+      node = new Node(val)
+    }
+
+    if (!this.top) {
+      this.top = node
+      this.bottom = node
+    } else {
+      const temp = this.top
+      this.top = node
+      this.top.next = temp
+    }
+    return ++this.length
+  }
+
+  pop() {
+    if (!this.top) {
+      return null
+    }
+
+    const temp = this.top
+    if (this.top === this.bottom) {
+      this.bottom = null
+    }
+    this.top = this.top.next
+    this.length--
+
+    return temp
+  }
+
+  size() {
+    return this.length
+  }
+}
+
+function iterateAcrossLinkedListBackwards(linkedList) {
+  if (!linkedList.length) return ''
+  const stack = new Stack()
+
+  let node = linkedList.head
+  stack.push(node)
+
+  while (node.next) {
+    node = node.next
+    stack.push(node)
+  }
+
+  let output = []
+
+  while (stack.size()) {
+    output.push(stack.pop().value)
+  }
+
+  return output
+    .map(e => {
+      if (e === null) return 'null'
+      if (e === undefined) return 'undefined'
+      if (e === false) return 'false'
+      return e
+    })
+    .join(' -> ')
+}
+
+exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards
