ab232e2c23e6f3eb7dcecafab34907c99b2acbf4
richierob62 <richierob62@gmail.com>
2019-08-10 18:57:21 -0400

    Add title

--- phone_interviews/71lbs/questions_71lbs.rb
+++ phone_interviews/71lbs/questions_71lbs.rb
@@ -58,6 +58,7 @@ def scrape(city)
   paragraphs = parsed_page.css("p.result-info")
   output = paragraphs.map do |p|
     [
+      p.css("a.result-title").map(&:text)[0],
       p.css("a.result-title").map { |link| link["href"] }[0],
       p.css("span.result-meta span.result-price").map(&:text)[0],
       p.css("span.result-meta span.result-hood").map(&:text)[0],
